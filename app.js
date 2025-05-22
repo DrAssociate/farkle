10a8672c259dc2102170e155b31461a07de98645
Bobby Sanchez <browniebandet@gmail.com>
2025-05-22 15:49:14 -0500

    working on computer's logic

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
+  ////////////////////////////////////////
+  //////////////GLOBAL LOGIC//////////////
+  ////////////////////////////////////////
+
   const throwBtn = document.querySelector('#throw-btn');
   const throwAgain = document.querySelector('#throw-again');
   const endTurn = document.querySelector('#end-turn');
@@ -20,6 +24,9 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#die6'),
   ];
 
+  // SETTING CURRENT PLAYER AS USER TO INITIATE GAME
+  let currentPlayer = 'user';
+
   // SETTING DEFAULT SCORE VALUES
   let globalCompScore = 0;
   let globalUserScore = 0;
@@ -37,42 +44,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // GLOBAL ROLLSCORE FOR KEEPING TRACK OF CURRENTROLLSCORE
   let currentRollScore = 0;
 
-  // INITIAL THROW BTN FUNCTION
-  throwBtn.addEventListener('click', function () {
-    currentRollScore = 0;
-
-    // DISPLAYING THE ROLLED DIE ON SCREEN
-    // dice.forEach((die) => {
-    //   let dieNumber = Math.floor(Math.random() * 6) + 1;
-    //   die.value = dieNumber;
-    //   die.style.display = 'block';
-    //   die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
-    //   counts[dieNumber]++;
-    // });
-
-    // FOR DEBUGGING BUST ROLL
-    // const bustValues = [2, 3, 4, 6, 2, 4]; // any combo with no 1s, no 5s, no 3-of-a-kind
-    // dice.forEach((die, index) => {
-    //   const dieNumber = bustValues[index];
-    //   die.value = dieNumber;
-    //   die.style.display = 'block';
-    //   die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
-    //   counts[dieNumber]++;
-    // });
-
-    // FOR DEBUGGING ALL DICE SCORE
-    const bustValues = [1, 1, 1, 1, 1, 1]; // any combo with no 1s, no 5s, no 3-of-a-kind
-    dice.forEach((die, index) => {
-      const dieNumber = bustValues[index];
-      die.value = dieNumber;
-      die.style.display = 'block';
-      die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
-      counts[dieNumber]++;
-    });
-
-    // CHECKING FOR VALID THROW
-    checkForValidThrow(counts, 'initial-roll');
-  });
+  ////////////////////////////////////////
+  //////////////GENERAL LOGIC/////////////
+  ////////////////////////////////////////
 
   // SHOWS SELECTED DIE AND ADD SCORE
   dice.forEach((die) => {
@@ -140,10 +114,158 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  // FOR BUSTED ROLL ATTEMPT
+  function checkForValidThrow(counts, source) {
+    const hasOne = counts[1] > 0;
+    const hasFive = counts[5] > 0;
+    const hasThreeOfAkind = Object.values(counts).some((count) => count >= 3);
+
+    if (hasOne || hasFive || hasThreeOfAkind) {
+      throwBtn.style.display = 'none';
+      throwAgain.style.display = 'block';
+    } else {
+      if (source === 'initial-roll') {
+        throwBtn.textContent = 'Sorry, you BUST';
+        throwBtn.disabled = true;
+        dice.forEach((die) => {
+          die.classList.add('locked');
+        });
+        return;
+      }
+      if (source === 'reroll') {
+        // LOGIC FOR REROLL BUST
+        throwAgain.textContent = 'Sorry, you BUST';
+        throwAgain.disabled = true;
+        dice.forEach((die) => {
+          die.classList.add('locked');
+        });
+        return;
+      }
+    }
+
+    // LOGIC FOR WHEN ALL THROWN DIE HAVE BEEN SCORED
+    // AND DICE RESET IS POSSIBLE
+    const allDiceScored = dice.every(
+      (die) =>
+        die.classList.contains('locked') ||
+        die.parentElement.id.includes('userHoldingContainer')
+    );
+
+    if (allDiceScored) {
+      resetDiceForReroll();
+    }
+  }
+
+  // MAKING SURE REROLL IS NOT PERMITTED UNLESS SCORE HAS BEEN MADE
+  function throwAgainEligible() {
+    if (currentRollScore > 0) {
+      throwAgain.classList.remove('locked');
+    } else {
+      throwAgain.classList.add('locked');
+    }
+  }
+
+  // // FUNCTION FOR RESETTING DICE AFTER SCORING WITH
+  // // ALL DIE
+  function resetDiceForReroll() {
+    dice.forEach((die) => {
+      dieNumbers.appendChild(die);
+      die.classList.remove('locked', 'chosen');
+      const newValue = Math.floor(Math.random() * 6) + 1;
+      die.value = newValue;
+      counts[newValue]++;
+      die.style.backgroundImage = `url(images/die${newValue}.png)`;
+      die.style.display = 'block';
+    });
+
+    currentRollScore = 0;
+    userScore.textContent = globalUserScore;
+    throwAgain.classList.remove('locked');
+    throwBtn.style.display = 'none';
+    throwAgain.style.display = 'block';
+  }
+
+  // FUNCTION FOR ENDING CURRENTPLAYER'S TURN
+  endTurn.addEventListener('click', () => {
+    // counts[0] = 0;
+    // counts[1] = 0;
+    // counts[2] = 0;
+    // counts[3] = 0;
+    // counts[4] = 0;
+    // counts[5] = 0;
+    // console.log(counts);
+
+    dice.forEach((die) => {
+      die.classList.add('locked');
+      // die.classList.remove('locked');
+      die.classList.remove('chosen');
+
+      if (currentPlayer === 'user') {
+        currentPlayer = 'computer';
+        throwBtn.classList.add('locked');
+        throwBtn.style.display = 'none';
+        throwAgain.style.display = 'none';
+        endTurn.style.display = 'none';
+      } else {
+        currentPlayer = 'user';
+      }
+
+      if (die.parentElement === userHoldingContainer) {
+        userHoldingContainer.removeChild(die);
+        dieNumbers.appendChild(die);
+      }
+      die.style.display = 'none';
+    });
+
+    setTimeout(() => {
+      computerTurn();
+    }, 1500);
+  });
+
+  ////////////////////////////////////////
+  ///////////////USER LOGIC///////////////
+  ////////////////////////////////////////
+
+  // INITIAL THROW BTN FUNCTION
+  throwBtn.addEventListener('click', function () {
+    currentRollScore = 0;
+    console.log('User is playing');
+
+    // DISPLAYING THE ROLLED DIE ON SCREEN
+    dice.forEach((die) => {
+      let dieNumber = Math.floor(Math.random() * 6) + 1;
+      die.value = dieNumber;
+      die.style.display = 'block';
+      die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
+      counts[dieNumber]++;
+    });
+
+    // FOR DEBUGGING BUST ROLL
+    // const bustValues = [2, 3, 4, 6, 2, 4]; // any combo with no 1s, no 5s, no 3-of-a-kind
+    // dice.forEach((die, index) => {
+    //   const dieNumber = bustValues[index];
+    //   die.value = dieNumber;
+    //   die.style.display = 'block';
+    //   die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
+    //   counts[dieNumber]++;
+    // });
+
+    // FOR DEBUGGING ALL DICE SCORE
+    // const bustValues = [1, 1, 1, 1, 1, 1]; // any combo with no 1s, no 5s, no 3-of-a-kind
+    // dice.forEach((die, index) => {
+    //   const dieNumber = bustValues[index];
+    //   die.value = dieNumber;
+    //   die.style.display = 'block';
+    //   die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
+    //   counts[dieNumber]++;
+    // });
+
+    // CHECKING FOR VALID THROW
+    checkForValidThrow(counts, 'initial-roll');
+  });
+
   // // THROW AGAIN BTN FUNCTION
   throwAgain.addEventListener('click', () => {
-    console.log(`You pressed the throw again button`);
-
     throwAgain.classList.add('locked');
 
     // Move chosen dice to holding container and lock them
@@ -188,77 +310,57 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
 
-    console.log(`Remaining die values below:`);
-    console.log(counts);
-
     checkForValidThrow(counts, 'reroll');
   });
 
-  // FOR BUSTED ROLL ATTEMPT
-  function checkForValidThrow(counts, source) {
-    console.log(`Checking here`, counts);
+  ////////////////////////////////////////
+  /////////////COMPUTER LOGIC/////////////
+  ////////////////////////////////////////
 
-    const hasOne = counts[1] > 0;
-    const hasFive = counts[5] > 0;
-    const hasThreeOfAkind = Object.values(counts).some((count) => count >= 3);
-
-    if (hasOne || hasFive || hasThreeOfAkind) {
-      throwBtn.style.display = 'none';
-      throwAgain.style.display = 'block';
-    } else {
-      if (source === 'initial-roll') {
-        throwBtn.textContent = 'Sorry, you BUST';
-        throwBtn.disabled = true;
-      }
-      if (source === 'reroll') {
-        // LOGIC FOR REROLL BUST
-        throwAgain.textContent = 'Sorry, you BUST';
-        throwAgain.disabled = true;
-      }
-    }
-
-    // LOGIC FOR WHEN ALL THROWN DIE HAVE BEEN SCORED
-    // AND DICE RESET IS POSSIBLE
-    const allDiceScored = dice.every(
-      (die) =>
-        die.classList.contains('locked') ||
-        die.parentElement.id.includes('userHoldingContainer')
-    );
-
-    console.log(`Checking before rest`, counts);
-    if (allDiceScored) {
-      resetDiceForReroll();
-    }
-  }
-
-  // MAKING SURE REROLL IS NOT PERMITTED UNLESS SCORE HAS BEEN MADE
-  function throwAgainEligible() {
-    if (currentRollScore > 0) {
-      throwAgain.classList.remove('locked');
-    } else {
-      throwAgain.classList.add('locked');
-    }
-  }
-
-  // // FUNCTION FOR RESETTING DICE AFTER SCORING WITH
-  // // ALL DIE
-  function resetDiceForReroll() {
-    dice.forEach((die) => {
-      dieNumbers.appendChild(die);
-      die.classList.remove('locked', 'chosen');
-      const newValue = Math.floor(Math.random() * 6) + 1;
-      die.value = newValue;
-      counts[newValue]++;
-      die.style.backgroundImage = `url(images/die${newValue}.png)`;
-      die.style.display = 'block';
-    });
+  function computerTurn() {
+    console.log(`Computer's turn begins!`);
 
     currentRollScore = 0;
-    userScore.textContent = globalUserScore;
-    throwAgain.classList.remove('locked');
-    throwBtn.style.display = 'none';
-    throwAgain.style.display = 'block';
-  }
 
-  // endTurn.addEventListener('click', () => {});
+    // KEEPING TRACK OF CURRENT COUNT WHILE CHOOSING DICE
+    const counts = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+    };
+
+    dice.forEach((die) => {
+      let dieNumber = Math.floor(Math.random() * 6 + 1);
+      die.value = dieNumber;
+      die.style.display = 'block';
+      die.style.backgroundImage = `url(images/die${dieNumber}.png)`;
+      counts[dieNumber]++;
+    });
+
+    let diceToKeep = [];
+
+    // FOR SCORING 1's 5's OR 3 OF A KIND
+    for (let i = 1; i <= 6; i++) {
+      if (counts[i] >= 3) {
+        let multiplier = 1;
+        if (counts[i] === 4) multiplier === 2;
+        if (counts[i] === 5) multiplier === 3;
+        if (counts[i] === 6) multiplier === 4;
+
+        if (i === 1) {
+          currentRollScore += 1000 * multiplier;
+        } else {
+          currentRollScore += i * 100 * multiplier;
+        }
+
+        counts[i] = 0;
+      }
+    }
+
+    currentRollScore += counts[1] * 100;
+    currentRollScore += counts[5] * 50;
+  }
 });
